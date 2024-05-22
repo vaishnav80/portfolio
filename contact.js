37a7eb9e6cd2e5304079ce10f9ed59424dd5a211
vaishnav80 <vaishnavpuzhakkal3@gmail.com>
2024-05-22 14:24:46 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,13 @@
+// function sendMail(){
+//     alert("fhbdhbf");
+//     let parms={
+//         name :document.getElementById("name").value,
+//         email : document.getElementById("email").value,
+//         subject : document.getElementById("subject").value,
+//         message : document.getElementById("message").value
+        
+//     }
+     
+//     emailjs.send("service_gri0oun","template_kgeboxa",parms).then(alert("email Sent!!"));
+
+// // }
